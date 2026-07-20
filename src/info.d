8ab29fed31b5f747b62b95c846861fd7a772acaf
Elgumi01 <miguelbarbosa2409@gmail.com>
2026-07-20 14:07:34 -0300

    chore: add .gitignore

--- src/info.d
+++
@@ -1,2 +0,0 @@
-src/info.o: src/info.c include/config.h
-include/config.h:
--- src/main.d
+++
@@ -1,4 +0,0 @@
-src/main.o: src/main.c include/info.h include/config.h include/config.h
-include/info.h:
-include/config.h:
-include/config.h:
